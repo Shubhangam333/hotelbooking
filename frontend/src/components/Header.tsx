ce1fbbad17946f8b502656ba165767c93ee4b317
Shubhangam333 <shubhangam27@gmail.com>
2024-01-09 17:13:59 +0530

    feat:adds registration functionality

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,19 +1,32 @@
 import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
   return (
-    <div className="bg-red-600 py-6">
+    <div className="bg-blue-800 py-6">
       <div className="container mx-auto flex justify-between">
         <span className="text-3xl text-white font-bold tracking-light">
           <Link to="/">HotelBooking</Link>
         </span>
         <span className="flex space-x-2">
-          <Link
-            to="/sign-in"
-            className="flex items-center bg-white rounded-lg px-3 text-red-600 font-bold hover:bg-orange-600 hover:text-white"
-          >
-            Sign In
-          </Link>
+          {isLoggedIn ? (
+            <>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-blue-600"
+                to="/my-bookings"
+              >
+                My Bookings
+              </Link>
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+            >
+              Sign In
+            </Link>
+          )}
         </span>
       </div>
     </div>
