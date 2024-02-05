eb382827ffc9fd4708814f1fb4595161d3af1cfa
Shubhangam333 <shubhangam27@gmail.com>
2024-02-05 16:15:38 +0530

    feat:add hotel creation functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,16 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+import myHotelRoutes from "./routes/my-hotels";
 import cookieParser from "cookie-parser";
+
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -21,6 +30,7 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
 
 app.listen(5000, () => {
   console.log("Server is running on PORT 5000");
